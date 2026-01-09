31e8601f37d0a7d99ddc88194bbfcec91313a94a
Ayman Boujjar <aymenboujjar12@gmail.com>
2026-01-09 12:44:50 +0100

    ddd

--- src/constants/trusted.ts
+++ src/constants/trusted.ts
@@ -1,6 +1,7 @@
 import jadara from "../assets/images/trusted/jadaralogo.png";
 import lionsgeek from "../assets/images/trusted/lionsgeeklogo.png";
 import casamemoire from "../assets/images/trusted/casamemorelogo.png";
+import ateliera1 from "../assets/images/trusted/atliera1.png";
 
 
 export const trusted = [
@@ -18,5 +19,10 @@ export const trusted = [
         name: 'Casa Mémoire',
         image: casamemoire,
         website: 'https://www.casamemoire.org/'
+    },
+    {
+        name: 'Atelier A1',
+        image: ateliera1,
+        website: 'https://ateliera1.com/'
     },
 ];
